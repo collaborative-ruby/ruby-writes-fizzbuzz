2a72f3d3ce30be91618affe3207ffa1684810cbd
Odin Dutton <odindutton@gmail.com>
2015-11-11 11:37:05 +1100

    ''''''''

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # 0_dea was here.
 1.upto 100 do |n|
- print "Fizz#{(n%3).zero? ? ?B+"z"*2}"if n % 5 ==0
+ print "Fizz#{(n%3).zero? ? ?'B+"z"*2}"if n % 5 ==0
  print "Buzz" if n%5<1
  print n u#"FizzBuz"
  #else
