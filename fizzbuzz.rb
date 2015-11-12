159e1794ee7a7f41b1047bd9da638903471e0f01
Tyler Thrailkill <tylerbthrailkill@gmail.com>
2015-11-12 15:19:49 -0700

    soo close

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 1.upto 100 do |n|
  print "Fizz"if (n%3).zero? # ?B+'u'+"z"*2:''}"if n % 3 ==0
  print "Buzz" if n%5<1
- print n unless (n %5)*n%3)#"FizzBuz"
+ print n unless (n %5)*(n%3)#"FizzBuz"
  #else
 p
  #end
