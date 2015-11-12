4fbe399da7c42b5422cab25a7d066c481b63d62c
Tom Lord <lord.thom@gmail.com>
2015-11-12 23:04:18 +0000

    Stop deleting my work

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # 0_dea was here.
 1.upto 100 do |n|
- print "Fizz" if (n%3).zero? # ?B+'u'+"z"*2:''}"if n % 3 ==
+ print "Fizz" if (n%3).zero? # ?B+'u'+"z"*2:''}"if n % 3 ==0
  print "Buzz" if n%5<1&&
  print n unless (n%5)*(n%3)<1#"FizzBuz"
  #else
