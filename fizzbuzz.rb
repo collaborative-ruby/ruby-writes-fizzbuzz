8522ff25f279a037da2c0c17180821e9732fb177
D.E. Akers <0x0dea@gmail.com>
2015-11-12 17:57:12 -0500

    Tom, plz! Let's clean this up and call it a great success.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 1.upto 100 do |n|
  print "Fizz" if (n%3).zero? # ?B+'u'+"z"*2:''}"if n % 3 =
  print "Buzz" if n%5<1
- print n unless (n %5)*(n%3)<1#"FizzBuz"
+ print n unless (n%5)*(n%3)<1#"FizzBuz"
  #else
  puts
  #end
